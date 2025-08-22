139ce7fcbcf720f687bbbfceed09806208dac7e1
Cris Schwartz <cris.schwartz0@gmail.com>
2025-08-22 13:56:48 -0500

    Initial script

---
+++ Script/Pathways_Analysis_250822.R
@@ -0,0 +1,7 @@
+# Title:    Analysis of CoE Student Pathways Data August 2025
+# File:     Pathways_Analysis_250822.R
+# Project:  Pathways_2025
+
+# CLEAN AND CLEAR THE ENVIRONMENT --------------------------
+rm(list = ls())             ## Clear environment
+# cat("\014")                 ## Clear console, ctrl+L
