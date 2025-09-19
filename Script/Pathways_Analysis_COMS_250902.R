3871135ae462baa8dc2f993a33d4a9b754006402
Cris Schwartz <cris.schwartz0@gmail.com>
2025-09-19 15:13:24 -0500

    Restructured the ggplot commands to remove them from being embedded within print command. Issue the print command explicitly after object is created. Also removed other plot attempts.

--- Script/Pathways_Analysis_COMS_250902.R
+++ Script/Pathways_Analysis_COMS_250902.R
@@ -65,7 +65,7 @@ coms_pathways_sankey_format <-
   node_fields %>% 
   make_long(major_first, degree_field)
 
-print(
+coms_sankey_plot <- # build ggplot object without plotting yet
   ggplot(coms_pathways_sankey_format %>% 
            mutate(node = fct_rev(as.factor(node)),
                   next_node = fct_rev(as.factor(next_node))
@@ -84,55 +84,5 @@ print(
           , axis.ticks = element_blank()  
           , panel.grid = element_blank()) 
   
-)
 
-bar_n  <- 50        # how many cases the ruler should represent
-x_pos  <- 0.6       # place near the left of the first stage (tweak as needed)
-y0     <- 0
-y1     <- bar_n
-
-print(
-  ggplot(
-    coms_pathways_sankey_format,
-    aes(x = x, next_x = next_x, node = node, next_node = next_node, fill = node, value = 1)
-  ) +
-    geom_sankey(flow.alpha = 0.8, node.color = "gray90", show.legend = FALSE) +
-    geom_sankey_label(aes(label = node), size = 3, color = "black", fill = "gray90") +
-    
-    # scale bar (doesn't inherit sankey aesthetics)
-    geom_segment(aes(x = x_pos, xend = x_pos, y = y0, yend = y1),
-                 inherit.aes = FALSE, linewidth = 0.6) +
-    geom_text(aes(x = x_pos, y = y1, label = paste0(bar_n, " cases")),
-              inherit.aes = FALSE, vjust = -0.4, size = 3) +
-    
-    coord_cartesian(clip = "off") +
-    theme_bw() +
-    theme(
-      legend.position = "none",
-      axis.title = element_blank(),
-      axis.text   = element_blank(),
-      axis.ticks  = element_blank(),
-      panel.grid  = element_blank(),
-      plot.margin = margin(5.5, 22, 5.5, 5.5) # a little room for the label
-    )
-)
-
-library(scales)
-print(
-  ggplot(
-    coms_pathways_sankey_format,
-    aes(x = x, next_x = next_x, node = node, next_node = next_node, fill = node, value = 1)
-  ) +
-    geom_sankey(flow.alpha = 0.8, node.color = "gray90", show.legend = FALSE) +
-    geom_sankey_label(aes(label = node), size = 3, color = "black", fill = "gray90") +
-    scale_y_continuous(name = "Count", breaks = pretty_breaks(n = 5),
-                       expand = expansion(mult = c(0.02, 0.02))) +
-    theme_bw() +
-    theme(
-      legend.position = "none",
-      axis.title.x = element_blank(),
-      axis.text.x  = element_blank(),
-      axis.ticks.x = element_blank(),
-      panel.grid   = element_blank()
-    )
-)
+print(coms_sankey_plot) # plot the ggplot object
