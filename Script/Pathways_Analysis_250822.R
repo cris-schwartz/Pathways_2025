59ed51caed5dac63262a9dbee4fa8a19774ac4f1
Cris Schwartz <cris.schwartz0@gmail.com>
2025-10-31 14:43:46 -0500

    imports pathway summary data from other script. Categorizing undeclareds

--- Script/Pathways_Analysis_250822.R
+++ Script/Pathways_Analysis_250822.R
@@ -10,3 +10,15 @@ rm(list = ls())             ## Clear environment
 pacman::p_load(magrittr, pacman, tidyverse,ggforce)
 library(readxl)
 # if (!require("ggsankey")) devtools::install_github("davidsjoberg/ggsankey") # install sankey package
+
+# LOAD AND PREPARE DATA ------------------------------------
+pathway_summary <- # import the previously prepared pathway_summary csv file
+  read_csv("./Data/Student_Pathway_Summary_250902.csv", guess_max = 1000) %>% # guess_max ensures empty rows not treated as logical values 
+  as_tibble()
+
+pathway_summary <- 
+  pathway_summary %>% 
+  mutate(undeclared_start = if_else( # categorize whether started CoE as undeclared, 1 is yes
+    (major_first == "Undeclared Engineering"), 1, 0
+  )) %>% 
+  relocate(undeclared_start, .after = coe_duration) # move column to better position
