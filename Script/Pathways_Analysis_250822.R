63557064b3f9295f0b339160d02f430f2538c844
Cris Schwartz <cris.schwartz0@gmail.com>
2025-11-06 13:21:53 -0600

    Added undeclared analysis

--- Script/Pathways_Analysis_250822.R
+++ Script/Pathways_Analysis_250822.R
@@ -22,3 +22,29 @@ pathway_summary <-
     (major_first == "Undeclared Engineering"), 1, 0
   )) %>% 
   relocate(undeclared_start, .after = coe_duration) # move column to better position
+
+outcome_resolved <- # identify students who have a resolved degree status
+  pathway_summary %>% 
+  filter(degree_outcome != 'Undetermined') 
+  
+# ANALYSIS OF UNDECLARED STUDENTS ####
+undeclared_starts <- 
+  outcome_resolved %>% 
+  filter(undeclared_start == 1)
+
+outcome_by_transfer_history <- # determine how many times student changed majors
+  undeclared_starts %>% 
+  group_by(major_changes, degree_outcome) %>% 
+  summarise(n = n())
+
+outcome_by_retention <- # determine how many graduated and in what college
+  undeclared_starts %>% 
+  group_by(degree_outcome, graduated_college) %>% 
+  summarise(n = n())
+  
+never_declared <- 
+  undeclared_starts %>% 
+  filter(is.na(major_second)) %>% 
+  group_by(degree_outcome, graduated_college) %>% 
+  summarise(n = n())
+  
